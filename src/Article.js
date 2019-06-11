7d574cb6f3c02a21b3514b087a6588c0a0935988
Harshil Agrawal <ghagrawal17@gmail.com>
2019-06-11 16:39:20 +0530

    code cleanup

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, WebView } from 'react-native'
+
+const articles ={
+    0:{
+        title: 'Privacy Month',
+        url: 'https://blog.harshil.dev/blog/privacy-month/'
+    },
+    1:{
+        title: 'FOSSASIA Summit 2019',
+        url: 'https://blog.harshil.dev/blog/fossasia-summit-2019-my-first-international-talk/'
+    },
+    2:{
+        title: 'Booth Crawl',
+        url: 'https://blog.harshil.dev/blog/booth-crawl-at-makerfest-vadodara-2019/'
+    },
+    3:{
+        title: 'JS Arrow Functions',
+        url: 'https://blog.harshil.dev/blog/js-arrow-functions-made-easy/'
+    },
+    4:{
+        title: 'Flutter Calculator',
+        url: 'https://blog.harshil.dev/blog/let\'s-flutter:-how-i-built-a-simple-calculator-with-flutter/'
+    },
+}
 
 export default class Article extends React.Component {
     static navigationOptions = {
@@ -7,7 +30,11 @@ export default class Article extends React.Component {
     }
 
     render() {
-        console.log(this.props.navigation.state.params)
-        return(<View><Text>hello {this.props.navigation.state.params.id}</Text></View>)
+        const { id } = this.props.navigation.state.params
+            if(!articles[id])
+                return(<View style={{flex:1}}><WebView source={{uri: `${articles[id].url}`}} /></View>)
+            else
+                return(<Text>Sorry Couldn't find the article</Text>)
+        
     }
 }
--- src/Home.js
+++ src/Home.js
@@ -4,34 +4,7 @@ export default class Home extends React.Component {
     static navigationOptions = {
         title:'Home'
     }
-
-    componentDidMount() {
-        if(Platform.OS==='android'){
-            Linking.getInitialURL().then(url=>{
-                console.log(url)
-                // this.navigate(url);
-            })
-        }
-        Linking.addEventListener('url', this._handleOpenURL);
-      }
-      componentWillUnmount() {
-        Linking.removeEventListener('url', this._handleOpenURL);
-      }
-      _handleOpenURL(event) {
-        console.log(event.url);
-      }
-
-    //   navigate = (url) => { // E
-    //     const { navigate } = this.props.navigation;
-    //     const route = url.replace(/.*?:\/\//g, '');
-    //     const id = route.match(/\/([^\/]+)\/?$/)[1];
-    //     const routeName = route.split('/')[0];
-      
-    //     if (routeName === 'article') {
-    //       navigate('Article', { id})
-    //     };
-    //   }
-      
+     
 
     render() {
         return(<View><Text>Home</Text></View>)
